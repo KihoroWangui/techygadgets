07480b041f79281594f753d5e07b0cefcbfc9c7f
Kihoro Wangui <94446783+KihoroWangui@users.noreply.github.com>
2026-01-13 16:44:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const Products =[
+        {id:1,name:"iphone 12",price:"Ksh35,000",image:"\images\iphone12.png"}
+        {id:2,name:"iphone 13",price:"Ksh60,000",image:"\images\iphone13.png"}
+    ]
   return (
     <div>
     <h1>Techy Gadgets</h1>
@@ -15,7 +19,7 @@ function App() {
     <h2>Laptops</h2>
     </div>
     </section>
-    
+    </div>
   );
 }
 
