eb0c78b8cf0e4b0a685a185ec68e0e1296b711c2
Kihoro Wangui <94446783+KihoroWangui@users.noreply.github.com>
2026-01-14 14:34:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-    const Products =[
-        {id:1, name:"iphone 12", price:"ksh35,000", image:"\images\iphone12.png"},
-        {id:2, name:"iphone 13", price:"Ksh60,000", image:"\images\iphone13.png"},
-    ]
+    
   return (
     <div className="App">
     {/*header*/}
